beaad0340fe2f91d9b822c2e5a98db502def93c3
Anton Zvonovskiy <Anton.Zvonovskiy@macys.com>
2021-01-31 18:47:28 +0300

    Updated time format and logic for upcoming games.

--- libs/epic_query_lib.py
+++ libs/epic_query_lib.py
@@ -2,6 +2,12 @@ URL = 'https://www.epicgames.com/store/ru/free-games'
 from libs.items import EpicGame
 from requests import get
 import json
+from datetime import datetime
+import pytz
+
+__RUS_TIME_FORMAT = "%d.%m.%Y %H:%M"
+__DATE_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
+__local_timezone = pytz.timezone("Europe/Moscow")
 
 def query_epic_games():
     """
@@ -14,23 +20,44 @@ def query_epic_games():
     items = json.loads(src)['data']['Catalog']['searchStore']['elements']
 
     epic_games = []
+    items = list((item for item in items if item['promotions']))
 
-    for item in items[:2]:
+    promotionalItems = list(item for item in items if item['promotions']['promotionalOffers'])
+    upcomingItems = list(item for item in items if item['promotions']['upcomingPromotionalOffers'])
+
+    promotionalItems = sorted(promotionalItems, key=lambda x: datetime.strptime(
+        x['promotions']['promotionalOffers'][0]['promotionalOffers'][0]['startDate'], __DATE_FORMAT))
+    upcomingItems = sorted(upcomingItems, key=lambda x: datetime.strptime(
+        x['promotions']['upcomingPromotionalOffers'][0]['promotionalOffers'][0]['startDate'], __DATE_FORMAT))
+
+    for item in promotionalItems:
         item_price = item['price']['totalPrice']['fmtPrice']['originalPrice']
         item_name = item['title']
-        if (len(epic_games) > 0 ):
-            item_img = EpicGame.NO_IMAGE_TAG
-            item_message = "Скоро появится"
-            effective_date = item['effectiveDate'][0:10]
-            epic_games[0].item_status_message = f"Сейчас бесплатно (до {effective_date})"
-        else:
-            item_img = item['keyImages'][0]['url']
-            item_message = "Сейчас бесплатно"
+        item_img = item['keyImages'][0]['url']
+        item_end_date = item['promotions']['promotionalOffers'][0]['promotionalOffers'][0]['endDate']
+        item_end_date = datetime.strptime(item_end_date, __DATE_FORMAT).replace(tzinfo=pytz.utc).astimezone(__local_timezone).strftime(__RUS_TIME_FORMAT)
+
+        item_message = f"Сейчас бесплатно до {item_end_date} (по Москве)"
 
         epic_game = EpicGame(item_price=item_price,
                              item_img=item_img,
                              item_status_message=item_message,
                              item_name=item_name)
+        epic_games.append(epic_game)
+
+    for item in upcomingItems:
+        item_price = item['price']['totalPrice']['fmtPrice']['originalPrice']
+        item_name = item['title']
+        item_img = EpicGame.NO_IMAGE_TAG
+        item_start_date = item['promotions']['upcomingPromotionalOffers'][0]['promotionalOffers'][0]['startDate']
+        item_start_date = datetime.strptime(item_start_date, __DATE_FORMAT).replace(tzinfo=pytz.utc).astimezone(__local_timezone).strftime(__RUS_TIME_FORMAT)
+
+        item_message = f"Скоро появится, с {item_start_date} (по Москве)"
+
+        epic_game = EpicGame(item_price=item_price,
+                         item_img=item_img,
+                         item_status_message=item_message,
+                         item_name=item_name)
         epic_games.append(epic_game)
 
     return epic_games
