cbf2e1ba7be2e8b3a2492716c98c116707a17179
Anton Zvonovskiy <Anton.Zvonovskiy@macys.com>
2021-05-27 20:25:56 +0300

    Changed polling to infinite polling.

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
-import telebot
-import config
-from libs import epic_query_lib
+from telebot import TeleBot
+from config import TOKEN
+from libs.epic_query_lib import query_epic_games
 
-bot = telebot.TeleBot(config.TOKEN)
+bot = TeleBot(TOKEN)
+
 
 @bot.message_handler(commands=['ts'])
 def ts_msg(message):
 	bot.send_message(message.chat.id, "79.173.84.84 - адрес сервера TS3\n".format(message.from_user, bot.get_me()),
 		parse_mode='html')
 
+
 @bot.message_handler(commands=['help'])
 def help_msg(message):
 	bot.send_message(message.chat.id, "Список команд для бота <b>{1.first_name}</b>:\n/epic - бесплатная игра в Epic Store\n/ts - адрес сервера TS3\n".format(message.from_user, bot.get_me()),
 		parse_mode='html')
 
+
 @bot.message_handler(commands=['epic'])
 def epic_msg(message):
-	games = epic_query_lib.query_epic_games(cached_game_infos)
+	games = query_epic_games(cached_game_infos)
 	pretty = ""
 	for game in games:
 		pretty += game.pretty_print_me()
 
 	bot.send_message(message.chat.id,"<b>{0.first_name}</b>, забирай:".format(message.from_user, bot.get_me()) + pretty, parse_mode='html')
+
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
@@ -31,6 +35,7 @@ def welcome(message):
 	bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот созданный чтобы быть подопытным кроликом.".format(message.from_user, bot.get_me()),
 		parse_mode='html')
 
+
 if __name__ == '__main__':
-	cached_game_infos = epic_query_lib.query_epic_games([])
-	bot.polling(none_stop=True, timeout=120)
+	cached_game_infos = query_epic_games([])
+	bot.infinity_polling(none_stop=True, timeout=120)
