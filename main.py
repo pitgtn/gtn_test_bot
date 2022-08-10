c88b159106ef9fe8f7de4b6dea1e0189e94430ce
pitgtn <pitgtn@yandex.ru>
2022-08-10 21:53:41 +0300

    Add def run_bot() with exception handling. Clear code.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telebot import TeleBot
 from config import TOKEN
 from libs.epic_query_lib import query_epic_games
 import time
-
+from datetime import datetime
 
 bot = TeleBot(TOKEN)
 
@@ -31,28 +31,25 @@ def epic_msg(message):
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
-	sti = open('staticcccc/wwelcome.webp', 'rb')
+	sti = open('static/welcome.webp', 'rb')
 	bot.send_sticker(message.chat.id, sti)
 
 	bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот созданный чтобы быть подопытным кроликом.".format(message.from_user, bot.get_me()),
 		parse_mode='html')
 
 
-# if __name__ == '__main__':
-# 	cached_game_infos = query_epic_games([])
-# 	bot.polling(none_stop=True, timeout=120)
+def run_bot():
+	try:
+		bot.polling(none_stop=True, timeout=120)
+	except Exception as e:
+		current_date = datetime.now().date()
+		with open(f'log_bot_{current_date}.txt', 'a') as f:
+			f.write(f'{e} \n')
+		f.close()
+		time.sleep(5)
+	run_bot()
+
 
 if __name__ == '__main__':
-	tim=time
-	with open('log_bot', 'a') as f:
-		f.write(f'{time} \n')
-	f.close()
 	cached_game_infos = query_epic_games([])
-	while True:
-		try:
-			bot.polling(none_stop=True, timeout=120)
-		except Exception as e:
-			time.sleep(3)
-			with open('log_bot', 'a') as f:
-				f.write(f'{e} \n')
-			f.close()
+	run_bot()
